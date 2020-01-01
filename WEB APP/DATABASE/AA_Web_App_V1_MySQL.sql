9b8321dd78365b00142961ade2c53c3d0d27fd49
nnwijesuriya <51411336+nnwijesuriya@users.noreply.github.com>
2020-01-01 15:19:31 +0530

    phase 2.4
    Co-Authored-By: IshanC7 <thenooobc7@users.noreply.github.com>
    Co-Authored-By: Lucas Lahiru Hasanka <hvlhasanka@users.noreply.github.com>
    Co-Authored-By: Ranul <lranul@users.noreply.github.com>
    Co-Authored-By: ryancargan <ryancargan@users.noreply.github.com>

--- WEB APP/DATABASE/AA_Web_App_V1_MySQL.sql
+++ WEB APP/DATABASE/AA_Web_App_V1_MySQL.sql
@@ -21,6 +21,17 @@ CREATE TABLE Driver(
 )ENGINE=INNODB;
 
 -- Inserting records into Table 1 - Driver
+INSERT INTO Driver( NIC, FirstName, LastName, DateOfBirth, Email, ContactNo, InsuranceNo, InsuranceName, LicenseNo)
+Values (2001345796, "Rohan", "Thomas", "1980-02-16", "Rohan12@gmail.com", 0763489516, "AB603759A", "Celinco Insurance", "JB6412"),
+(2001546861, "Peter", "Dissanayake", "1970/06/14", "Peter1@yahoo.com", 0729373776, "DC415615B", "AIG Insurance", "SL6721"),
+(3873439372, "Jackson", "Fernando", "1980/09/22", "fjackson@gmail.com", 0717672404, "FG841545V", "Softlogic insurance", "KJ2831"),
+(3408672316, "Kalani", "Silva", "1991/12/20", "Kalanisilva@yahoo.com", 0762863478, "XZ541651D", "FairFirst Insurance", "MC3670"),
+(7835404537, "Gihan", "Correa", "1999/02/26", "Gihan24@yahoo.com", 0762453387, "RT489651T", "Allianz Insurance", "TY3869"),
+(9643045334, "Akila", "Herath", "1975/06/02", "Herath14@yahoo.com", 0727683547, "UB448615I", "Celinco insurance", "LV3883"),
+(1042876039, "Albert", "Jayewardene", "1988/09/11", "albert54@gmail.com", 0718686388, "KG735445L", "AIG Insurance" ,"AC8874"),
+(8762858764, "Hasan", "Bandara", "1994/07/18", "bandara21@gmail.com", 0761427867, "UI498794S", "Allianz Insurance" ,"IO3635"),
+(5786227481, "Pasindu", "Ekanayake", "2000/12/14", "Pasindu41@yahoo.com", 0762586878, "MN145488V", "Allianz Insurance" ,"MK2785"),
+(7456780465, "Ajith", "Sirisena", "2001/02/18", "ajith@gmail.com", 0727873389, "AG784186C", "Celinco Insurance", "PA9046");
 
 
 -- Creating Table 2 - Report
@@ -41,6 +52,11 @@ CREATE TABLE Report(
 ALTER TABLE Report AUTO_INCREMENT = 0000001;
 
 -- Inserting records into Table 2 login- Report
+INSERT INTO Report(NIC, Severity, Type, Description, Date_Time, Longitude, Latitude)
+VALUES (7835404537, 1, "full", "A small accident with a small amount of damage", "2019/02/18 07:15:25", 79.980607, 6.834806),
+(5786227481, 3, "Third-party", "An accident with many injuries", "2019/012/30 09:58:49", 80.583487, 7.254586),
+(7456780465, 2, "full", "A major accident with many injuries", "2019/10/11 11:03:52", 80.991042, 6.838624);
+
 
 
 -- Creating Table 3 - ReportMedia
@@ -106,6 +122,17 @@ CREATE TABLE Police_Agent(
 )ENGINE=INNODB;
 
 -- Inserting records into Table 6 - Police_Agent
+INSERT INTO Police_Agent(NIC, Name, Email, ContactNo, EID)
+VALUES (2016341860, "Sahan", "Shan12@gmail.com", 0763619401, 9000001),
+(4891656189, "Oshan", "woshan@gmail.com", 0721416462, 9000002),
+(2165189489, "Ashane", "ashane41@gmail.com", 0761348431, 9000003),
+(9415618913, "Athula", "Athula7@yahoo.com", 0713184169, 9000004),
+(9526223252, "Chamara", "chamaraw@yahoo.com", 0713189494, 9000005),
+(8423210065, "Iranga", "Iranag13@gmail.com", 0721841984, 9000006),
+(8451122198, "Chaminda", "Chaminda3@yahoo.com", 0761816218, 9000007),
+(1349841297, "Lasitha", "Lasitha65@yahoo.com", 0711894923, 9000008),
+(7156213980, "Sandun", "Sandun62@yahoo.com", 0724984949, 9000009),
+(8451016196, "Sanjeeva", "Sanjeeva8@gmail.com", 0716498348, 9000010);
 
 
 -- Creating Table 7 - Insurance_Agent
@@ -120,6 +147,18 @@ CREATE TABLE Insurance_Agent(
 )ENGINE=INNODB;
 
 -- Inserting records into Table 7 - Insurance_Agent
+INSERT INTO Insurance_Agent(NIC, Name, Email, EID, InsuranceName)
+VALUES(7841561006, "Ajantha", "ajantha12@gmail.com", 0761848923, 90000011, "Celinco Insurance"),
+(4484521004, "Rajitha", "Rajitha31@yahoo.com", 0761984123, 90000012, "AIG Insurance"),
+(9485158941, "Ramesh", "Rames3@yahoo.com", 0713843831, 90000013, "Celinco insurance"),
+(5961489561, "Nuwan", "Nuwan8@gmail.com", 0726898499, 90000014, "Allianz Insurance"),
+(5962303894, "Udana", "Udana54@yahoo.com", 0763498423, 90000015, "Celinco insurance"),
+(2659264529, "Gamini", "Gamini314@yahoo.com", 0716884921, 90000016, "AIG Insurance"),
+(2051514121, "Ranga", "tranga@gmail.com", 0761348984, 90000017, "Allianz Insurance"),
+(3226448941, "Tharindu", "Tharindu98@gmail.com", 0719873428, 90000018, "AIG Insurance"),
+(1949168441, "Kamal", "Kamal7@yahoo.com", 0728792129, 90000019, "Celinco insurance"),
+(4897457846, "Pavan" , "Pavan1@yahoo.com", 0761423890, 90000020, "Allianz Insurance");
+
 
 
 -- Creating Table 8 - RDA_Agent
@@ -133,6 +172,17 @@ CREATE TABLE RDA_Agent(
 )ENGINE=INNODB;
 
 -- Inserting records into Table 8 - RDA_Agent
+INSERT INTO RDA_Agent(NIC, Name, Email, EID)
+VALUES(4894198994, "Chamara", "chamara24@gmail.com", 90000021),
+(7481984123, "Chathura", "wchathura@yahoo.com", 90000022),
+(4986516165, "Banduka", "banduka23@yahoo.com", 90000023),
+(84911849123, "Akith", "Akith64@gmail.com", 90000024),
+(3164946139, "Andrew", "Andrew74@gmail.com", 90000025),
+(7415112056, "Dulan", "Dulan13@yahoo.com", 90000026),
+(6321984891, "Vishal", "Vishal9@yahoo.com", 90000027),
+(0561489418, "Thilak", "thilak34@gmail.com", 90000028),
+(4135494138, "Marvan", "Marvan792@yahoo.com", 90000029),
+(8431257632, "Lavindra", "Lavindra476@yahoo.com", 90000030);
 
 
 -- Creating Table 9 - WebMaster
