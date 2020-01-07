d5d6f53a425cdcc91b3fda1e8467fbbed9bbc7ee
Lucas Lahiru.H.H <hvlhasanka@students.nsbm.lk>
2020-01-07 12:00:26 +0530

    Frontend
    Improved the user interface of the Police Webpage (police.php file).

--- Website/RDA/police.php
+++ Website/RDA/police.php
@@ -1,7 +1,27 @@
+<!--
+  Police Webpage coded by H.V.L.Hasanka
+-->
+
+<?php
+
+  // Including the database connection file
+//  include_once("connect.php");
+
+  // Retriving the number of new reports
+//  $result = mysqli_query($conn, "SELECT * FROM persons WHERE id=$id");
+
+//  while($res = mysqli_fetch_array($result))
+
+?>
+
 <!DOCTYPE html>
 <html>
-
 <head>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <title>RDA AMD</title>
@@ -17,58 +37,125 @@
     <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
     <link rel="stylesheet" href="assets/css/styles.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
-</head>
 
+    <!-- JavaScript code for Notification Alert -->
+    <script>
+    $(document).ready(function(){
+      $('[data-toggle="popover"]').popover();
+    });
+    </script>
+
+</style>
+</head>
 <body>
     <div></div>
     <div class="card"></div>
     <div>
-        <div class="header-blue" style="background-color: rgb(0,37,82);background-size: contain;background-image: url(&quot;X&quot;);">
+        <div class="header-blue" style="background-color: #99ddff;background-size: contain;background-image: url(&quot;X&quot;);">
             <nav class="navbar navbar-light navbar-expand-md navigation-clean-search">
-                <div class="container-fluid"><a class="navbar-brand" href="#"><strong>RDA Accident Management - POLICE&nbsp;</strong><br></a><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
-                    <div
-                        class="collapse navbar-collapse" id="navcol-1">
-                        <form class="form-inline mr-auto" target="_self">
-                            <div class="form-group"><label for="search-field"></label></div>
-                        </form><a class="btn btn-light action-button" role="button" href="#">Sign Out</a></div>
-        </div>
-        </nav>
+                <div class="container-fluid">
+                  <a class="navbar-brand" href="#">
+                    <p
+                      style="font-family: 'Open Sans', sans-serif;
+                             letter-spacing: 1.5px;
+                             font-size: 25px;
+                             color: black;">
+                      <strong>RDA Accident Management - POLICE&nbsp;</strong><br>
+                    </p>
+                  </a>
+                  <button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="navbar-toggler-icon"></span>
+                  </button>
+                  <div class="collapse navbar-collapse" id="navcol-1">
+                      <form class="form-inline mr-auto" target="_self">
+                          <div class="form-group"><label for="search-field"></label></div>
+                      </form>
+                      <a class="btn btn-light action-button" role="button" href="#"
+                        style="color: black; border-color: black;">
+                        Sign Out</a>
+                  </div>
+              </div>
+            </nav>
+            <!-- Bootstrap Spinner -->
+            <div style="position: absolute;
+                        left: 50%;
+                        transform: translate(-50%, -0%);">
+              <div class="spinner-grow text-light"
+                style="width: 100px;
+                       height: 100px;">
+              </div>
+            </div>
+            <!-- Bell Notification Icon -->
+            <a href="#" data-toggle="popover" data-trigger="focus" title="New Notifications" data-content="######" data-placement="bottom"
+              style="position: absolute;
+                     top: 75px;
+                     left: 95%;
+                     transform: translate(-5%, -0%);">
+              <i class="fas fa fa-bell"
+                style="font-size: 30px;
+                       color: black;">
+              </i>
+            </a>
+      </div>
     </div>
-    </div>
-    <div style="background-color: rgb(8,49,129);">
-        <div class="container">
+
+    <!-- Bootstrap Cards -->
+    <div style="background-color: #66ccff;">
+      <p style="text-align: center;
+                font-size: 30px;
+                padding-top: 15px;
+                font-family: 'Open Sans', sans-serif"><b>Current Statistics</b></p>
+        <div class="container"
+          style="padding-bottom: 10px;">
             <div class="row">
                 <div class="col-md-3">
-                    <div class="card">
-                        <div class="card-body text-white bg-primary" data-bs-hover-animate="pulse">
-                            <h4 class="text-center card-title">Registered Users</h4><small class="form-text text-uppercase text-center text-white">150 users</small></div>
+                    <div class="card"
+                      style="border-radius: 20px;">
+                        <div class="card-body text-white bg-primary" data-bs-hover-animate="pulse"
+                          style="border-radius: 20px;">
+                            <h4 class="text-center card-title">Registered Users</h4>
+                            <small class="form-text text-uppercase text-center text-white">150 users</small>
+                        </div>
                     </div>
                 </div>
                 <div class="col-md-3">
-                    <div class="card">
-                        <div class="card-body text-white bg-danger" data-bs-hover-animate="pulse">
-                            <h4 class="text-center text-white card-title">Accidents</h4><small class="form-text text-uppercase text-center text-white">300 ACCIDENTS</small></div>
+                    <div class="card"
+                      style="border-radius: 20px;">
+                        <div class="card-body text-white bg-danger" data-bs-hover-animate="pulse"
+                          style="border-radius: 20px;">
+                            <h4 class="text-center text-white card-title">Accidents</h4>
+                            <small class="form-text text-uppercase text-center text-white">300 ACCIDENTS</small>
+                        </div>
                     </div>
                 </div>
                 <div class="col-md-3">
-                    <div class="card">
-                        <div class="card-body bg-success" data-bs-hover-animate="pulse">
-                            <h4 class="text-center text-white card-title">Managed</h4><small class="form-text text-center text-white">10 MANAGED ACCIDENTS</small></div>
+                    <div class="card"
+                      style="border-radius: 20px;">
+                        <div class="card-body bg-success" data-bs-hover-animate="pulse"
+                          style="border-radius: 20px;">
+                            <h4 class="text-center text-white card-title">Managed</h4>
+                            <small class="form-text text-center text-white">10 MANAGED ACCIDENTS</small>
+                        </div>
                     </div>
                 </div>
-                <div class="col-md-3" data-bs-hover-animate="pulse">
-                    <div class="card text-white bg-warning">
-                        <div class="card-body bg-warning">
-                            <h4 class="text-center card-title">Ongoing</h4><small class="form-text text-center text-white">50 ATTENDING ACCIDENTS</small></div>
+                <div class="col-md-3"  data-bs-hover-animate="pulse">
+                    <div class="card text-white bg-warning"
+                      style="border-radius: 20px;">
+                        <div class="card-body bg-warning"
+                          style="border-radius: 20px;">
+                            <h4 class="text-center card-title">Ongoing</h4>
+                            <small class="form-text text-center text-white">50 ATTENDING ACCIDENTS</small>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    <div class="highlight-blue" style="background-color: #0b0d39;">
+    <div class="highlight-blue" style="background-color:#4d88ff;">
         <div class="container-fluid">
             <div class="intro">
-                <h2 class="text-uppercase text-center">RDA AMD - POLICE PORTAL</h2>
+                <h2 class="text-uppercase text-center">Road Development Authority(RDA) AMD - POLICE PORTAL</h2>
             </div>
         </div>
     </div>
@@ -77,8 +164,25 @@
             <div class="row">
                 <div class="col-auto col-md-12 text-center align-self-center m-auto" style="background-color: rgb(8,49,129);">
                     <nav class="navbar navbar-light navbar-expand-xl navigation-clean-button" style="background-color: rgb(8,49,129);">
-                        <div class="container"><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
-                            <div class="collapse navbar-collapse" id="navcol-1"><span class="navbar-text actions"> </span><span class="navbar-text actions"> <a class="btn btn-primary bg-success action-button" role="button" href="#" style="margin: 10px;">Manage Accident Reports</a></span></div>
+                        <div class="container">
+                          <button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1">
+                            <span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span>
+                          </button>
+                          <!-- Bootstrap NavBar -->
+                          <div class="collapse navbar-collapse" id="navcol-1">
+                            <span class="navbar-text actions">
+                              <!-- Bootstrap Collapse -->
+                              <button type="button" class="btn btn-primary bg-success action-button" data-toggle="collapse" data-target="#viewStatisticsContainer">View Statistics on Accidents</button>
+                                <div id="viewStatisticsContainer" class="collapse">
+                                  <button type="button" class="btn btn-info btn-block" style="margin-top: 20px;
+                                                                                              border-radius: 10px;">Type</button>
+                                  <button type="button" class="btn btn-info btn-block" style="margin-top: 20px;
+                                                                                              border-radius: 10px;">Location</button>
+                                  <button type="button" class="btn btn-info btn-block" style="margin-top: 20px;
+                                                                                              border-radius: 10px;">Past and Present Fluctuation</button>
+                                </div>
+                            </span>
+                          </div>
                         </div>
                     </nav>
                 </div>
@@ -88,7 +192,7 @@
     <div class="text-white bg-dark map-clean">
         <div class="container">
             <div class="intro">
-                <h2 class="text-center">Map</h2>
+                <h2 class="text-center">Map of Current Accidents</h2>
             </div>
         </div><iframe allowfullscreen="" frameborder="0" src="https://www.google.com/maps/embed/v1/place?key=AIzaSyDaEw6o8OhJvRQnTF3gI_tibMejtfasOlY&amp;q=7.8731%2C+80.7718&amp;zoom=7" width="100%" height="450"></iframe></div>
     <div class="highlight-blue" style="background-color: rgb(8,49,129);"></div>
